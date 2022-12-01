ee5879982513ee605c012331ff15681e602bd20c
JordonGiusto <jordon.giusto@gmail.com>
2022-12-01 07:44:22 -0800

    edited stuff

--- expressServer/server.js
+++ expressServer/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-const port = 3001;
+const port = process.env.PORT || 3000;
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../glTesting')));
 
 app.listen(port, () => {
     console.log(`Server is listening at port ${port}`);   
