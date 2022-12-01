85e21a8a08735fea7ec04ce8f16d7ae766d67f7a
JordonGiusto <jordon.giusto@gmail.com>
2022-12-01 07:49:20 -0800

    edited stuff

--- expressServer/server.js
+++ expressServer/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../glTesting')));
