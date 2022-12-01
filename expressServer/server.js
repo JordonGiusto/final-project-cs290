008e57164cca02420ada53e616d81e5de58df184
JordonGiusto <jordon.giusto@gmail.com>
2022-12-01 07:58:06 -0800

    edited stuff

--- expressServer/server.js
+++ expressServer/server.js
@@ -8,11 +8,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../glTesting')));
 
-while (port < 4000) {
-    try {
-        app.listen(port, () => console.log(`Listening on port ${port}`));
-        break;
-    } catch (err) {
-        port++;
-    }
-}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
